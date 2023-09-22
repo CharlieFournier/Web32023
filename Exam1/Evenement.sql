dccf154fc57df125cb1430f3b0f7911ee5792fc8
koguwo <shamanking2001.ql@gmail.com>
2023-09-22 09:31:22 -0400

    bd evenement

--- Exam1/Evenement.sql
+++ Exam1/Evenement.sql
@@ -1,11 +1,11 @@
-git-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.5.4.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 19 Septembre 2023 à 21:28
+-- Généré le :  Ven 22 Septembre 2023 à 13:21
 -- Version du serveur :  5.7.11
--- Version de PHP :  5.6.18
+-- Version de PHP :  7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,20 +27,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `evenement` (
-    `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nomEvent` varchar(30) NOT NULL,
   `description` varchar(200) NOT NULL,
   `date` date NOT NULL,
   `lieu` varchar(20) NOT NULL,
-  `departement` varchar(20) NOT NULL
+  `departement` varchar(20) NOT NULL,
+  `positif` int(3) NOT NULL,
+  `neutre` int(3) NOT NULL,
+  `rage` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `evenement`
 --
 
-INSERT INTO `evenement` (`id`, `nomEvent`, `description`, `date`, `lieu`, `departement`) VALUES
-(1, 'Pizza Stage', 'AH', '2023-09-12', 'sous-sol', 'aucun');
+INSERT INTO `evenement` (`id`, `nomEvent`, `description`, `date`, `lieu`, `departement`, `positif`, `neutre`, `rage`) VALUES
+(1, 'Pizza Stage', 'AH', '2023-09-12', 'sous-sol', 'aucun', 2, 0, 0);
 
 --
 -- Index pour les tables exportées
