660a479f8c3c36cf58b63972b2a1190184a63a87
CharlieFournier <charliefournier01@hotmail.com>
2023-10-03 16:33:02 -0400

    finission de la page ajout

--- Exam1/AjoutEvents.php
+++ Exam1/AjoutEvents.php
@@ -3,18 +3,29 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="StyleModification.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
+
 <body>
     <?php
 
-    $nom = $image = "";
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $db = "meow";
+
+    $conn = new mysqli($servername, $username, $password, $db);
+
+
+    $nomEvent = $date = $lieu = $departement = $description = "";
 
 
     $nomErreur = "";
@@ -24,37 +35,150 @@ session_start();
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Si on entre, on est dans l'envoie du formulaire
-        
-        if(empty($_POST['nom'])){
-            $nomErreur = "Le nom est requis";
+
+        if (empty($_POST['nomEvent'])) {
+            $nomErreur = "Le nom de l'event est requis";
             $erreur = true;
+        } else {
+            $nomEvent = test_input($_POST["nomEvent"]);
         }
-        else{
-            $nom = test_input($_POST["nom"]); 
+
+        if (empty($_POST['date'])) {
+            $nomErreur = "La date de l'event est requis";
+            $erreur = true;
+        } else {
+            $date = test_input($_POST["date"]);
         }
-        $image = test_input($_POST["image"]);
 
+        if (empty($_POST['departement'])) {
+            $nomErreur = "Le departement de l'event est requis";
+            $erreur = true;
+        } else {
+            $departement = test_input($_POST["departement"]);
+        }
 
-        // Inserer dans la base de données
-        //SI erreurs, on réaffiche le formulaire 
+        if (empty($_POST['description'])) {
+            $nomErreur = "La description de l'event est requis";
+            $erreur = true;
+        } else {
+            $description = test_input($_POST["description"]);
+        }
+
+        if (empty($_POST['lieu'])) {
+            $nomErreur = "Le lieu de l'event est requis";
+            $erreur = true;
+        } else {
+            $lieu = test_input($_POST["lieu"]);
+        }
+
+        //---------------------------------------------------------------------------//
+        if ($erreur == false) {
+
+            $sql = "INSERT INTO `evenement` SET `nomEvent` = '$nomEvent', `departement` = '$departement', `date` = '$date', `lieu`='$lieu' , `description`='$description', `positif` = 0, `neutre` = 0, `rage` = 0, `positifEnt` = 0, `neutreEnt` = 0, `rageEnt` = 0";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "mise a jour effectuer correctement";
+                header('Location: ./PageModeration.php?action=modifier');
+            } else {
+                echo "erreur dans la mise a jour" . $conn->error;
+            }
+
+            if (!$conn) {
+                die("Connection failed: " . $mysqli_connect_error());
+            }
+
+            //---------------------------------------------------------------------------//
+
+        }
     }
-    if($_SERVER["REQUEST_METHOD"] != "POST" || $erreur == true) {
+    if ($_SERVER["REQUEST_METHOD"] != "POST" || $erreur == true) {
+
+        if ($erreur == true) {
+        } ?>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top p-0">
+            <div class="container-fluid navbar p-0">
+
+                <a class="navbar-brand p-0" href="https://www.cegeptr.qc.ca/" target="_blank"><img src="Cegep3rLogo.jpg" id="logoNavBar"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active navText" aria-curent="page" href="PageModeration.php">Page d'accueil</a>
+                        <a class="nav-link navText" href="PageUser.php">Users</a>
+                        <a class="nav-link navText" href="PageEvents.php">Évènements</a>
+                        <a class="nav-link navText" href="index.php">Page Vote</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="test1 container-fluid text-center h-100 ">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
 
 
-    ?>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-            Nom : <input type="text" name="nom" size="25" maxlength="15"><br>
-            <span style="color:red";><?php echo $nomErreur;?></span><br><br>
-            
-            Image : <input type="text" name="image" value="<?php echo $image; ?>"><br>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="was-validated row g-2" novalidate>
 
-            <input type="submit">
-        </form>
+                        <div class="col-md-4">
+                            <label for="validationServer01" class="form-label">nomEvent</label>
+                            <input type="text" class="form-control is-valid" id="validationServer01" value="" name="nomEvent" required>
+                        </div>
+
+                        <div class="col-md-4">
+
+                        </div>
+
+                        <div class="col-md-4">
+
+
+                            <label for="validationServer01" class="form-label">departement</label>
+                            <input type="text" class="form-control is-valid" id="validationServer03" value="" name="departement" required>
+
+                        </div>
+
+                        <span style="color:red" ;><?php echo $nomErreur; ?></span><br>
+
+                        <div class="col-md-4">
+                            <label for="validationServer01" class="form-label">date</label>
+                            <input type="date" class="form-control is-valid" id="validationServer04" value="" name="date" required>
+                        </div>
+
+                        <div class="col-md-4"></div>
+
+                        <div class="col-md-4">
+                            <label for="validationServer01" class="form-label">lieu</label>
+                            <input type="text" class="form-control is-valid" id="validationServer05" value="" name="lieu" required>
+
+                        </div>
+
+                        <div class="col-12">
+                            <label for="validationServer01" class="form-label">Description</label>
+                            <input type="text" class="form-control is-valid" id="validationServer06" value="" name="description" required>
+
+                        </div>
+
+                        <hr>
+
+                        <input type="submit">
+                    </form>
+                </div>
+                <div class="col-3"></div>
+
+            </div>
+        </div>
+
+
+
+
 
     <?php
     }
 
-    function test_input($data){
+    function test_input($data)
+    {
         $data = trim($data);
         $data = addslashes($data);
         $data = htmlspecialchars($data);
@@ -63,6 +187,7 @@ session_start();
 
     ?>
 
-    
+
 </body>
+
 </html>
