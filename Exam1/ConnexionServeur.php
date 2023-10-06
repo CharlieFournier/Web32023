cdc3d3d0a51c230e116a4741e3fa6cde4b0acf6e
Charlie Fournier <charliefournier01@hotmail.com>
2023-10-06 08:49:41 -0400

    ah

--- Exam1/ConnexionServeur.php
+++ Exam1/ConnexionServeur.php
@@ -4,9 +4,12 @@
  $password = "2052524";
  $db = "a2023_420326_gr01_2052524-charlie-fournier";
  $conn = new mysqli($servername, $username, $password, $db);
+ 
  $sql = "SELECT * FROM evenement";
  $sql2 = "SELECT * FROM user";
+
 $result = $conn->query($sql);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
